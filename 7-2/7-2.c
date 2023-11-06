1c6b1380f391f5d6ee9085107a017a77b47ee405
BlueNyang <xpsj20@gmail.com>
2023-11-06 23:40:07 +0900

    ch 6 ch7

---
+++ 7-2/7-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <process.h>
+
+int print_num(int a) {
+	while (1) {
+		printf("%d\n", a);
+		Sleep(1000);
+	}
+}
+
+int main(void) {
+	_beginthreadex(NULL, 0, (_beginthreadex_proc_type)print_num, (void*)256, 0, NULL);
+	_beginthreadex(NULL, 0, (_beginthreadex_proc_type)print_num, (void*)128, 0, NULL);
+	Sleep(5000);
+
+	return 0;
+}
